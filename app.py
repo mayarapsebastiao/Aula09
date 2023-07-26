b56ab6ae8ba55423bad11d21313b8b47393cd59c
Mayara Sebastiao <mayara.psebastiao@gmail.com>
2023-07-26 14:09:31 -0300

    aula 09

---
+++ app.py
@@ -0,0 +1,40 @@
+#i = 1
+
+#while i < 10:
+#    print(i)
+#    i += 1
+#print("terminou")
+#print(i)
+
+criancas = ["Manu","Vini","Selina"]
+#for item in criancas:
+#    print(item)
+
+#canal = "Refatorando"
+#for letra in canal:
+#    print(letra)
+
+#for index in range(6,20,2):
+#    print(index)
+    
+#for index in range(len(criancas)):
+#    print(criancas[index],index)
+
+#for index in range(5):
+#    if index == 0:
+#        print("primeira linha")
+#    else:
+#        print(f"outras linhas {index}")
+
+matrix_numeros = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0]
+]
+
+for linha in matrix_numeros:
+    #print(linha)
+    print("----")
+    for coluna in linha:
+        print(coluna)
